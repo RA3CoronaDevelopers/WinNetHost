103973ab33408b168da5b55ba956a06460e24e19
Dynesshely <catrol@qq.com>
2022-05-29 23:55:53 +0800

    ❌ Test: 启动失败

--- source/NativeHost/nativehost.cpp
+++ source/NativeHost/nativehost.cpp
@@ -17,23 +17,11 @@
 #include <coreclr_delegates.h>
 #include <hostfxr.h>
 
-#ifdef WINDOWS
 #include <Windows.h>
 
 #define STR(s) L ## s
 #define CH(c) L ## c
 #define DIR_SEPARATOR L'\\'
-
-#else
-#include <dlfcn.h>
-#include <limits.h>
-
-#define STR(s) s
-#define CH(c) c
-#define DIR_SEPARATOR '/'
-#define MAX_PATH PATH_MAX
-
-#endif
 
 using string_t = std::basic_string<char_t>;
 
@@ -49,22 +37,13 @@ namespace
     load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly(const char_t *assembly);
 }
 
-#if defined(WINDOWS)
 int __cdecl wmain(int argc, wchar_t *argv[])
-#else
-int main(int argc, char *argv[])
-#endif
 {
     // Get the current executable's directory
     // This sample assumes the managed assembly to load and its runtime configuration file are next to the host
     char_t host_path[MAX_PATH];
-#if WINDOWS
     auto size = ::GetFullPathNameW(argv[0], sizeof(host_path) / sizeof(char_t), host_path, nullptr);
     assert(size != 0);
-#else
-    auto resolved = realpath(argv[0], host_path);
-    assert(resolved != nullptr);
-#endif
 
     string_t root_path = host_path;
     auto pos = root_path.find_last_of(DIR_SEPARATOR);
@@ -83,7 +62,7 @@ int main(int argc, char *argv[])
     //
     // STEP 2: Initialize and start the .NET Core runtime
     //
-    const string_t config_path = root_path + STR("DotNetLib.runtimeconfig.json");
+    const string_t config_path = root_path + STR("CoronaLauncher.runtimeconfig.json");
     load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer = nullptr;
     load_assembly_and_get_function_pointer = get_dotnet_load_assembly(config_path.c_str());
     assert(load_assembly_and_get_function_pointer != nullptr && "Failure: get_dotnet_load_assembly()");
@@ -91,10 +70,9 @@ int main(int argc, char *argv[])
     //
     // STEP 3: Load managed assembly and get function pointer to a managed method
     //
-    const string_t dotnetlib_path = root_path + STR("DotNetLib.dll");
-    const char_t *dotnet_type = STR("DotNetLib.Lib, DotNetLib");
-    const char_t *dotnet_type_method = STR("Hello");
-    // <SnippetLoadAndGet>
+    const string_t dotnetlib_path = root_path + STR("CoronaLauncher.dll");
+    const char_t *dotnet_type = STR("CoronaLauncher.EntryPoint, CoronaLauncher");
+    const char_t *dotnet_type_method = STR("Main");
     // Function pointer to managed delegate
     component_entry_point_fn hello = nullptr;
     int rc = load_assembly_and_get_function_pointer(
@@ -104,7 +82,6 @@ int main(int argc, char *argv[])
         nullptr /*delegate_type_name*/,
         nullptr,
         (void**)&hello);
-    // </SnippetLoadAndGet>
     assert(rc == 0 && hello != nullptr && "Failure: load_assembly_and_get_function_pointer()");
 
     //
@@ -112,54 +89,14 @@ int main(int argc, char *argv[])
     //
     struct lib_args
     {
-        const char_t *message;
-        int number;
+        wchar_t *message[];
     };
-    for (int i = 0; i < 3; ++i)
-    {
-        // <SnippetCallManaged>
-        lib_args args
-        {
-            STR("from host!"),
-            i
-        };
-
-        hello(&args, sizeof(args));
-        // </SnippetCallManaged>
-    }
-
-#ifdef NET5_0
-    // Function pointer to managed delegate with non-default signature
-    typedef void (CORECLR_DELEGATE_CALLTYPE *custom_entry_point_fn)(lib_args args);
-    custom_entry_point_fn custom = nullptr;
-    rc = load_assembly_and_get_function_pointer(
-        dotnetlib_path.c_str(),
-        dotnet_type,
-        STR("CustomEntryPointUnmanaged") /*method_name*/,
-        UNMANAGEDCALLERSONLY_METHOD,
-        nullptr,
-        (void**)&custom);
-    assert(rc == 0 && custom != nullptr && "Failure: load_assembly_and_get_function_pointer()");
-#else
-    // Function pointer to managed delegate with non-default signature
-    typedef void (CORECLR_DELEGATE_CALLTYPE *custom_entry_point_fn)(lib_args args);
-    custom_entry_point_fn custom = nullptr;
-    rc = load_assembly_and_get_function_pointer(
-        dotnetlib_path.c_str(),
-        dotnet_type,
-        STR("CustomEntryPoint") /*method_name*/,
-        STR("DotNetLib.Lib+CustomEntryPointDelegate, DotNetLib") /*delegate_type_name*/,
-        nullptr,
-        (void**)&custom);
-    assert(rc == 0 && custom != nullptr && "Failure: load_assembly_and_get_function_pointer()");
-#endif
-
     lib_args args
     {
-        STR("from host!"),
-        -1
+        *argv
     };
-    custom(args);
+
+    hello(&args, sizeof(args));
 
     return EXIT_SUCCESS;
 }
@@ -174,7 +111,6 @@ namespace
     void *load_library(const char_t *);
     void *get_export(void *, const char *);
 
-#ifdef WINDOWS
     void *load_library(const char_t *path)
     {
         HMODULE h = ::LoadLibraryW(path);
@@ -187,22 +123,7 @@ namespace
         assert(f != nullptr);
         return f;
     }
-#else
-    void *load_library(const char_t *path)
-    {
-        void *h = dlopen(path, RTLD_LAZY | RTLD_LOCAL);
-        assert(h != nullptr);
-        return h;
-    }
-    void *get_export(void *h, const char *name)
-    {
-        void *f = dlsym(h, name);
-        assert(f != nullptr);
-        return f;
-    }
-#endif
 
-    // <SnippetLoadHostFxr>
     // Using the nethost library, discover the location of hostfxr and get exports
     bool load_hostfxr()
     {
@@ -221,9 +142,7 @@ namespace
 
         return (init_fptr && get_delegate_fptr && close_fptr);
     }
-    // </SnippetLoadHostFxr>
 
-    // <SnippetInitialize>
     // Load and initialize .NET Core and get desired function pointer for scenario
     load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly(const char_t *config_path)
     {
@@ -249,5 +168,4 @@ namespace
         close_fptr(cxt);
         return (load_assembly_and_get_function_pointer_fn)load_assembly_and_get_function_pointer;
     }
-    // </SnippetInitialize>
 }
